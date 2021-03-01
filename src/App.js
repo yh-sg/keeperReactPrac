3b6ad2a3e44e36901f65c6c2baca04558678e3f7
yonghan <wangyh.92@gmail.com>
2021-03-02 07:22:03 +0800

    simple note mapping props react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './Components/Note.jsx'
 import Note from './Components/Note.jsx';
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
+import notes from './notes.js'
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note=>{
+        return(
+          <Note title={note.title} content={note.content}/>
+        )
+      })}
+      
       <Footer />
     </div>
   );
